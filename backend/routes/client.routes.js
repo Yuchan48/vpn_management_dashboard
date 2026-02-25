cd83482fd74d423c3a5120ec64eb113e3f070d97
Yuchan48 <yuchan.iizuka@gmail.com>
2026-02-25 17:39:03 +0100

    Implemented **routes/client.routes.js** that maps HTTP requests (`GET`, `POST`, `DELETE`) to controller functions for client management.

---
+++ backend/routes/client.routes.js
@@ -0,0 +1,10 @@
+/*
+Define endpoints
+Call controller functions
+*/
+
+const express = require("express");
+const router = express.Router();
+const clientController = require("../controllers/client.controller");
+
+router.get("/", clientController.getAllClients);

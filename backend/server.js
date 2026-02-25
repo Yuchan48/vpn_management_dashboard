13e3a694ac669a0f433951c43d673e785c18cb96
Yuchan48 <yuchan.iizuka@gmail.com>
2026-02-25 17:37:45 +0100

    added mounting`/clients` routes and error-handling middleware

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,19 @@
 */
 
 const express = require("express");
+
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5500;
 
+const errorHandler = require("./middleware/error.middleware");
+const clientRoutes = require("./routes/client.routes");
+
 app.use(express.json());
+
+// Mount routes at /clients
+app.use("/clients", clientRoutes);
 
 app.use(
   cors({
@@ -23,6 +30,9 @@ app.get("/status", (req, res) => {
   res.json({ status: "Running", timestamp: new Date().toISOString() });
 });
 
+// Error handling middleware. This should be after all routes to catch any errors that occur in the route handlers.
+app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
@@ -31,8 +41,8 @@ app.listen(port, () => {
 include error handling
 */
 
-app.get("/clients", (req, res) => {});
+/* app.get("/clients", (req, res) => {});
 
 app.post("/clients", (req, res) => {});
 
-app.delete("/clients/:id", (req, res) => {});
+app.delete("/clients/:id", (req, res) => {}); */

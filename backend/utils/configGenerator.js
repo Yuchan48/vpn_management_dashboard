6035c4aada98df78c54e98e66bd8d74f596dbf8a
Yuchan48 <yuchan.iizuka@gmail.com>
2026-03-03 22:58:45 +0100

    Generate .conf file

---
+++ backend/utils/configGenerator.js
@@ -0,0 +1,39 @@
+/*
+Function: generateClientConfig(client)
+- Input: client object with keys + IP
+- Output: string containing .conf content
+
+Controller endpoint: GET /clients/:id/config
+- Fetch client from DB
+- Call generateClientConfig()
+- Return file as download
+*/
+
+function generateClientConfig(client, privateKey) {
+  return `
+[Interface]
+PrivateKey = ${privateKey}
+Address = ${client.ip_address}/24
+DNS = ${process.env.DNS_SERVER}
+
+[Peer]
+PublicKey = ${process.env.SERVER_PUBLIC_KEY}
+Endpoint = ${process.env.SERVER_ENDPOINT}
+AllowedIPs = 0.0.0.0/0
+PersistentKeepalive = 25
+`.trim();
+}
+
+/*
+DNS is usually the VPN server or a public DNS server. Same for all clients.
+
+SERVER_PUBLIC_KEY and SERVER_ENDPOINT are the same for all clients, as they point to the VPN server.
+
+AllowedIPs is set to 0.0.0.0/0 to route all traffic through the VPN.
+
+PersistentKeepalive is optional but can help maintain the connection, especially for clients behind NAT.
+*/
+
+module.exports = {
+  generateClientConfig,
+};

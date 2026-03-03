e4349526505b1e15a8fdacfe5d617f104cef38f4
Yuchan48 <yuchan.iizuka@gmail.com>
2026-03-03 22:58:15 +0100

    method for assigning unique IP for new client

--- backend/utils/ipAllocator.js
+++ backend/utils/ipAllocator.js
@@ -1,6 +1,6 @@
 /*
-2. Add IP Address Allocation Logic
-You need a way to assign unique VPN IPs.
+IP Address Allocation Logic to assign unique VPN IPs for each client.
+
 Example VPN subnet:
 10.0.0.0/24
 
@@ -13,15 +13,37 @@ Clients:
 10.0.0.4
 ...
 
-Create:
-utils/ipAllocator.js
-
-Function:
-getNextAvailableIp()
-
 It should:
-- Query DB
 - Find highest assigned IP
 - Return next one
 
 */
+
+function getNextAvailableIp(clients) {
+  const baseIp = "10.0.0.";
+
+  // Extract assigned IPs from clients and convert to integers for comparison
+  const assignedIps = clients
+    .map((client) => client.ip_address)
+    .filter((ip) => ip !== null)
+    .map((ip) => parseInt(ip.split(".")[3], 10)); // Extract the last octet and convert to integer.
+  // returns an array of integers representing the last octet of assigned IPs, e.g., [2, 3, 4]
+
+  let nextIpSuffix = 2; // Start from 2 since 1 is typically reserved for the server
+
+  while (assignedIps.includes(nextIpSuffix)) {
+    nextIpSuffix++;
+
+    // If we exceed 254, it means we've run out of available IPs in the subnet
+    if (nextIpSuffix > 254) {
+      throw new Error("No available IP addresses in the subnet");
+    }
+  }
+
+  // Return the next available IP address in the format "10.0.0.X"
+  return `${baseIp}${nextIpSuffix}`;
+}
+
+module.exports = {
+  getNextAvailableIp,
+};

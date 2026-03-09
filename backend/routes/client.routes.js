5aa8993271359530f802ab1e0b433269ffb8e338
Yuchan48 <yuchan.iizuka@gmail.com>
2026-03-09 18:59:34 +0100

    unnecessary comments removed

--- backend/routes/client.routes.js
+++ backend/routes/client.routes.js
@@ -1,8 +1,3 @@
-/*
-Define endpoints
-Call controller functions
-*/
-
 const express = require("express");
 const router = express.Router();
 const clientController = require("../controllers/client.controller");
@@ -20,7 +15,3 @@ router.get("/:id/status", clientController.getClientStatus);
 router.get("/status", clientController.getAllClientsStatus);
 
 module.exports = router;
-
-/*
-The routes defined in this file are responsible for handling HTTP requests related to clients. Each route corresponds to a specific endpoint and HTTP method, such as GET for retrieving all clients, POST for creating a new client, and DELETE for removing a client by ID. The route handlers call the appropriate controller functions to perform the necessary operations and send back JSON responses to the client. This separation of concerns allows for cleaner code organization and easier maintenance.
-*/

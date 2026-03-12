1588f2ef57fa5fc18a31b7b6659d5b45b559dbca
Yuchan48 <yuchan.iizuka@gmail.com>
2026-03-12 11:12:53 +0100

    importing all user info instead of only user id

--- backend/middleware/requireAdmin.js
+++ backend/middleware/requireAdmin.js
@@ -1,11 +1,11 @@
 function requireAdmin(req, res, next) {
   // Check if the user role is present. If not, the user is not authenticated properly.
-  if (!req.userRole) {
+  if (!req.user.role) {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
   // Check if the user role is "admin". If not, deny access.
-  if (req.userRole !== "admin") {
+  if (req.user.role !== "admin") {
     return res.status(403).json({ error: "Forbidden: Admins only" });
   }
   next();

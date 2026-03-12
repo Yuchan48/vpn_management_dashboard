05daf97cf470fb4ec7284f207a3061fae65c0afb
Yuchan48 <yuchan.iizuka@gmail.com>
2026-03-12 11:13:11 +0100

    login limiter added for security

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { login } = require("../controllers/auth.controller");
+const { loginRateLimiter } = require("../middleware/rateLimit.middleware");
 
 // POST /api/auth/login
-router.post("/login", login);
+router.post("/login", loginRateLimiter, login);
 
 module.exports = router;

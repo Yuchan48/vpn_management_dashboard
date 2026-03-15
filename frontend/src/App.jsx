c0ca9f7b93bade84829991bd3b522c4327855ced
Yuchan48 <yuchan.iizuka@gmail.com>
2026-03-15 18:16:47 +0100

    change password page and route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,17 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 // Importing pages
 import Dashboard from "./pages/Dashboard";
 import LoginPage from "./pages/LoginPage";
+import ChangePassword from "./pages/ChangePassword";
 
 import ProtectedRoute from "./routes/ProtectedRoute";
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
+      <div className="App h-screen flex items-center justify-center bg-gray-700">
         <Routes>
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/change-password" element={<ChangePassword />} />
 
           {/* Protected route. Only accessible if authenticated. */}
           <Route
---
+++ frontend/src/pages/ChangePassword.jsx
@@ -0,0 +1,91 @@
+const ChangePassword = () => {
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="bg-gray-300 px-10 py-15 rounded-lg shadow-lg">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="text-center text-2xl/9 font-bold tracking-tight text-gray-800">
+            Change Your Password
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-3 text-gray-900">
+            <div>
+              <label
+                htmlFor="current-password"
+                className="block text-sm/6 font-medium "
+              >
+                Current Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="current-password"
+                  name="current-password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full border-1 border-gray-500 rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="new-password"
+                className="block text-sm/6 font-medium"
+              >
+                New Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="new-password"
+                  name="new-password"
+                  type="password"
+                  required
+                  autoComplete="new-password"
+                  className="block w-full border-1 border-gray-500 rounded-md bg-white/5 px-3 py-1.5 text-base outline-1 -outline-offset-1 outline-white/10 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="confirm-password"
+                className="block text-sm/6 font-medium"
+              >
+                Confirm New Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="confirm-password"
+                  name="confirm-password"
+                  type="password"
+                  required
+                  autoComplete="new-password"
+                  className="block w-full border-1 border-gray-500 rounded-md bg-white/5 px-3 py-1.5 text-base outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-700 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Change Password
+              </button>
+            </div>
+          </form>
+          <a
+            href="/login"
+            className="block mt-4 text-center text-sm/6 font-semibold text-indigo-700 hover:text-indigo-500"
+          >
+            Back to Login
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChangePassword;

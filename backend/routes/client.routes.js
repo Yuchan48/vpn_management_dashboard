4f3001f525715ee7bb4952c347f72b8f494a5e6c
Yuchan48 <yuchan.iizuka@gmail.com>
2026-03-22 13:23:17 +0100

    removed unnecessary routes in backend

--- backend/routes/client.routes.js
+++ backend/routes/client.routes.js
@@ -1,22 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const requireAdmin = require("../middleware/requireAdmin");
 const clientController = require("../controllers/client.controller");
 
 // Admin only routes
-router.get("/", requireAdmin, clientController.getAllClients);
+router.get("/", clientController.getClients);
 
-router.get("/status", requireAdmin, clientController.getAllClientsStatus);
-
-// Routes accessible to authenticated users (both admin and regular users)
 router.post("/", clientController.createClient);
-
-router.get("/me", clientController.getUserClients);
 
 router.delete("/:id", clientController.deleteClient);
 
 router.get("/:id/config", clientController.getClientConfig);
-
-router.get("/:id/status", clientController.getClientStatus);
 
 module.exports = router;

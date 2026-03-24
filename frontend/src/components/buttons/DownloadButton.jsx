7047fb868e822246b79f1fd1f94ed04e2cefc6eb
Yuchan48 <yuchan.iizuka@gmail.com>
2026-03-24 17:40:42 +0100

    margin fixed

--- frontend/src/components/buttons/DownloadButton.jsx
+++ frontend/src/components/buttons/DownloadButton.jsx
@@ -2,7 +2,7 @@ import Spinner from "../icons/Spinner";
 const DownloadButton = ({ onClick, disabled }) => {
   return (
     <button
-      className={`inline-flex items-center justify-center px-2 py-1 text-xs font-semibold text-indigo-600 border border-indigo-600 rounded-md disabled:text-gray-500 disabled:border-gray-500 disabled:cursor-not-allowed ${!disabled ? "hover:bg-indigo-100" : ""}`}
+      className={`inline-flex items-center justify-center px-1 py-1 text-xs font-semibold text-indigo-600 border border-indigo-600 rounded-md disabled:text-gray-500 disabled:border-gray-500 disabled:cursor-not-allowed ${!disabled ? "hover:bg-indigo-100" : ""}`}
       onClick={onClick}
       disabled={disabled}
     >

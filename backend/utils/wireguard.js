36f37dae1882da7eec1935ac0644a6032464e71e
Yuchan48 <yuchan.iizuka@gmail.com>
2026-03-29 19:44:44 +0200

    sudo added for wg commands

--- backend/utils/wireguard.js
+++ backend/utils/wireguard.js
@@ -2,12 +2,12 @@ const { execFileSync, exec } = require("child_process");
 
 function generateKeyPair() {
   // wg genkey generates a private key
-  const privateKey = execFileSync("wg", ["genkey"], {
+  const privateKey = execFileSync("sudo", ["wg", "genkey"], {
     encoding: "utf-8",
   }).trim();
 
   // wg pubkey generates the corresponding public key from the private key
-  const publicKey = execFileSync("wg", ["pubkey"], {
+  const publicKey = execFileSync("sudo", ["wg", "pubkey"], {
     input: privateKey,
     encoding: "utf-8",
   }).trim();

1a8301332c0a56e019c16f0981e5d7aec1f54df1
Yuchan48 <yuchan.iizuka@gmail.com>
2026-06-09 13:43:03 +0900

    fix: update socket connection to use API_BASE_URL for improved environment handling

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -4,9 +4,9 @@ import { io } from "socket.io-client";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // For production
-// const API_BASE_URL = "/api";
+// const API_BASE_URL = "/";
 
-const socket = io("/", {
+const socket = io(API_BASE_URL + "/", {
   path: "/socket.io",
   transports: ["websocket"],
   withCredentials: true,

38ce63e776bb9397b4b20c7d6f25797dd19fb397
Yuchan48 <yuchan.iizuka@gmail.com>
2026-08-22 22:45:39 +0200

    feat: add aria-hidden attributes for accessibility in icon components

--- frontend/src/components/icons/Spinner.jsx
+++ frontend/src/components/icons/Spinner.jsx
@@ -5,6 +5,7 @@ const Spinner = ({ className }) => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
+      aria-hidden="true"
     >
       <circle
         className="opacity-25"

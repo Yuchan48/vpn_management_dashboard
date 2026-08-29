855b2799c2f4589731b794de6ce7f17c12dd4c9d
Yuchan48 <yuchan.iizuka@gmail.com>
2026-08-30 00:08:50 +0200

    refactor: remove production API_BASE_URL from socket configuration

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -3,9 +3,6 @@ import { io } from "socket.io-client";
 // For development
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-// For production
-const API_BASE_URL = "/api";
-
 const socket = io("/", {
   path: "/socket.io",
   transports: ["websocket"],
